a957a78c6d06148b44e2afb25a15d128083a7b94
x-debug <coding2coding@qq.com>
2020-12-02 11:08:10 +0800

    :sunglasses: refactor the layout

--- Week02/api/authcontroller.go
+++ Week02/api/authcontroller.go
@@ -1,3 +1,34 @@
 package api
 
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/x-debug/Go-000/Week02/service"
+	"net/http"
+	"strconv"
+)
 
+func UserHandler(resp http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		sid := req.FormValue("uid")
+		uid, err := strconv.Atoi(sid)
+		if err != nil {
+			resp.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		user, err := service.GetUser(uint64(uid))
+		if err != nil {
+			resp.WriteHeader(http.StatusInternalServerError)
+			fmt.Printf("%+v", err)
+			err := errors.Cause(err)
+			_, _ = resp.Write([]byte(fmt.Sprintf("%s", err)))
+			return
+		}
+
+		resp.WriteHeader(http.StatusOK)
+		_, _ = resp.Write([]byte(fmt.Sprintf("%s", user)))
+		return
+	}
+
+	resp.WriteHeader(http.StatusNotImplemented)
+}
